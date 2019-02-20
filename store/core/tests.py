f81faa6f90857b8de11308579fa9255e3c0baf2e
Leonardo Gregório de Almeida <leogreal@gmail.com>
2019-02-19 21:24:27 -0300

    Refactoring to split tests in some class

--- store/core/tests.py
+++ store/core/tests.py
@@ -20,6 +20,7 @@ class ProductModelTest(TestCase):
         self.obj.save()
 
     def test_create(self):
+        """Ensure we can create a new product"""
         self.assertTrue(Product.objects.exists())
 
     def test_creation_date(self):
@@ -30,13 +31,14 @@ class ProductModelTest(TestCase):
         self.assertEqual('Running Shoes', str(self.obj))
 
 
-class ProductApiTest(APITestCase):
+class ProductApiGetTest(APITestCase):
 
     def setUp(self):
         Product.objects.create(
             name='Running Shoes',
             description='Soft and light running shoes.',
             price=100,)
+
         Product.objects.create(
             name='Blouse',
             description='Cotton blouse',
@@ -50,48 +52,80 @@ class ProductApiTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_all_products(self):
-        """Test GET all products"""
+        """Get /products/ all products must be returned"""
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         response = self.client.get(r('core:product-list'), format='json')
         self.assertEqual(response.data, serializer.data)
 
+
+class ProductApiCreateTest(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
     def test_create_product(self):
         """Ensure we can create a new product object."""
-        url = r('core:product-list')
         data_to_create = {'name': 'Bandana',
                           'description': 'Printed Bandana',
                           'price': '18.55'}
-        response = self.client.post(url, data_to_create, format='json')
+        response = self.client.post(
+            r('core:product-list'), data_to_create, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         for key in data_to_create:
             self.assertEqual(data_to_create[key], response.data[key])
 
+
+class ProductApiRetrieveTest(APITestCase):
+
+    def setUp(self):
+        self.pk = 1
+        self.product = Product.objects.create(
+            name='Running Shoes',
+            description='Soft and light running shoes.',
+            price=100,)
+
+        self.client = APIClient()
+
+    def test_get(self):
+        """Get /products/1/ must return status 200"""
+        response = self.client.get(
+            r('core:product-detail', pk=self.pk), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
     def test_retrieve_product(self):
         """Ensure we can retrieve a product object by id."""
-        pk = 1
-        product = Product.objects.get(id=pk)
-        serializer = ProductSerializer(product, many=False)
-
-        url = r('core:product-detail', pk=pk)
-        response = self.client.get(url, format='json')
+        serializer = ProductSerializer(self.product, many=False)
+        response = self.client.get(
+            r('core:product-detail', pk=self.pk), format='json')
         self.assertEqual(response.data, serializer.data)
+
+
+class ProductApiUpdateTest(APITestCase):
+
+    def setUp(self):
+        self.pk = 1
+        self.product = Product.objects.create(
+            name='Running Shoes',
+            description='Soft and light running shoes.',
+            price=100,)
+
+        self.client = APIClient()
+
+    def test_put(self):
+        """Put /products/1/ must return status 200"""
+        data_to_update = ProductSerializer(self.product, many=False).data
+        response = self.client.put(
+            r('core:product-detail', pk=self.pk), data_to_update, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_product(self):
         """Ensure we can update a product object by id."""
-        pk = 1
-        url = r('core:product-detail', pk=pk)
-        response = self.client.get(url, format='json')
-
-        data_to_update = response.data
+        data_to_update = ProductSerializer(self.product, many=False).data
         data_to_update['description'] = 'Bandana very stylish'
 
-        url = r('core:product-detail', pk=pk)
-        response = self.client.put(url, data_to_update, format='json')
+        response = self.client.put(
+            r('core:product-detail', pk=self.pk), data_to_update, format='json')
 
-        product = Product.objects.get(id=pk)
-        serializer = ProductSerializer(product, many=False)
-
-        self.assertEqual(response.data, serializer.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, data_to_update)
