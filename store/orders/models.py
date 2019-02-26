08adfe286a9bf531fb36460ee6fde18992ffff5a
Leonardo Gregório de Almeida <leogreal@gmail.com>
2019-02-25 22:26:53 -0300

    Ensure we can create a OrderItem

--- store/orders/models.py
+++ store/orders/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from store.authentication.models import Profile
+from store.products.models import Product
 
 
 class Order(models.Model):
@@ -11,6 +12,21 @@ class Order(models.Model):
 
     class Meta:
         db_table = 'order'
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    current_price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.IntegerField()
+
+    class Meta:
+        db_table = 'order_item'
 
     def __str__(self):
         return str(self.id)
--- store/orders/tests/test_model_order.py
+++ store/orders/tests/test_model_order.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 
+from store.products.models import Product
 from store.orders.models import Profile
-from store.orders.models import Order
+from store.orders.models import Order, OrderItem
 
 
 class OrderModelTest(TestCase):
@@ -13,9 +14,25 @@ class OrderModelTest(TestCase):
             name='Leonardo Gregório',
             email='leogreal@gmail.com')
 
-        self.obj = Order(user=self.user)
-        self.obj.save()
+        self.product = Product(
+            name='Running Shoes',
+            description='Soft and light running shoes.',
+            price=100,)
+        self.product.save()
 
-    def test_create(self):
+        self.order = Order(user=self.user)
+        self.order.save()
+
+        self.order_item = OrderItem(order=self.order,
+                                    product=self.product,
+                                    current_price=self.product.price,
+                                    quantity=1)
+        self.order_item.save()
+
+    def test_create_order(self):
         """Ensure we can create a new Order"""
         self.assertTrue(Order.objects.exists())
+
+    def test_create_item(self):
+        """Ensure we can create a OrderItem"""
+        self.assertTrue(OrderItem.objects.exists())
